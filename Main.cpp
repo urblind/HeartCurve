66977dccd8422ee0eb0bdab207369f6bc9193f40
urblind <vmxcyber@outlook.com>
2019-03-17 09:26:30 +0800

    I♥U, but you are blind.

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(void) 
+{
+	for (float y = 1.5f; y > -1.5f; y -= 0.1f) 
+	{
+		for (float x = -1.5f; x < 1.5f; x += 0.05f) 
+		{
+			float a = x * x + y * y - 1;
+			cout << (a * a * a - x * x * y * y * y <= 0.0f ? '*' : ' ');
+		}
+		cout << endl;
+	}
+
+	system("pause");
+	return 0;
+}
